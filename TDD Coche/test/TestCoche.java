242151b2ade617a7f846635a1b2d4c8ba307d512
CrisMH1 <116901383+CrisMH1@users.noreply.github.com>
2023-04-02 19:59:04 +0200

    Update TestCoche.java

--- TDD Coche/test/TestCoche.java
+++ TDD Coche/test/TestCoche.java
@@ -6,20 +6,20 @@ import org.junit.Test;
 public class TestCoche {
 
 	@Test
-	public void test_al_crear_un_coche_su_velocidad_es_cero() {
+	public void test_al_crear_un_coche_su_velocidad_es_ceroChristian() {
 		Coche nuevoCoche = new Coche();
 		Assert.assertEquals(0,nuevoCoche.velocidad);
 	}
 	
 	@Test
-	public void test_al_acelerar_un_coche_su_velocidad_aumenta() {
+	public void test_al_acelerar_un_coche_su_velocidad_aumentaChristian() {
 		Coche nuevoCoche = new Coche();
 		nuevoCoche.acelerar(30);
 		Assert.assertEquals(30,nuevoCoche.velocidad);
 	}
 	
 	@Test
-	public void test_al_decelerar_un_coche_su_velocidad_disminuye() {
+	public void test_al_decelerar_un_coche_su_velocidad_disminuyeChristian() {
 		Coche nuevoCoche = new Coche();
 		nuevoCoche.velocidad = 50;
 		nuevoCoche.decelerar(20);
@@ -27,7 +27,7 @@ public class TestCoche {
 	}
 	
 	@Test
-	public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_a_cero() {
+	public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_a_ceroChristian() {
 		Coche nuevoCoche = new Coche();
 		nuevoCoche.velocidad = 50;
 		nuevoCoche.decelerar(80);
